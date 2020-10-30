318b2bd5f901523c43646de90b68588b6cb7d7fd
juliavillela <33360385+juliavillela@users.noreply.github.com>
2020-10-30 15:48:08 -0300

    creates Note model and adds user to admin

--- notes/models.py
+++ notes/models.py
@@ -1,6 +1,25 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+from .helpers import find_first, order
 # Create your models here.
 class User(AbstractUser):
     pass
+
+class Note(models.Model):
+    title = models.CharField(max_length=300)
+    description = models.CharField(max_length=1200, blank=True, null=True)
+    public = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
+    content = models.CharField(max_length=10000, default="", blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    def serialize(self):
+        return {
+            "id": self.pk,
+            "title":self.title,
+            "description":self.description,
+            "user_id": self.user.id,
+            "content": self.content
+        }
